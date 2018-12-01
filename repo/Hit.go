9c2b516d0685ffbae809043349d72c883db83c80
makki0205 <llxo2_5oxll@icloud.com>
2018-12-01 14:36:20 +0900

    .

--- repo/Hit.go
+++ repo/Hit.go
@@ -1,5 +1,7 @@
 package repo
 
+import "fmt"
+
 var Hit = hitRepo{}
 
 // TODO 必要であれば排他処理を入れる
@@ -12,10 +14,12 @@ func (h *hitRepo) Get() int {
 }
 
 func (h *hitRepo) Reset() {
+	fmt.Println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!RESET!!!!!!!!!!!!!!!!!!!!!")
 	h.p = 0
 }
 
 func (h *hitRepo) Add(p int) int {
 	h.p += p
+	fmt.Println(h.Get())
 	return h.Get()
 }
