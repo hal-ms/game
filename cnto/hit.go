5b62eae055339960c066e9ac130efc7cdd0ad3bb
makki0205 <llxo2_5oxll@icloud.com>
2018-12-01 13:49:45 +0900

    .

--- cnto/hit.go
+++ cnto/hit.go
@@ -1,12 +1,18 @@
 package cnto
 
 import (
+	"fmt"
+
 	"github.com/hal-ms/game/repo"
 	"github.com/hal-ms/game/service"
 )
 
 func Hit(p int) {
 	if repo.State.Get().IsStandby {
+		fmt.Println(p)
+		if p > 60 {
+			service.GameStart()
+		}
 		return
 	}
 	if repo.State.Get().IsWearing {
--- service/button.go
+++ service/button.go
@@ -1,6 +1,28 @@
 package service
 
-var Button = btnService{}
+import (
+	"errors"
+	"fmt"
 
-type btnService struct {
+	"github.com/hal-ms/game/repo"
+	"github.com/makki0205/log"
+)
+
+func GameStart() {
+	if !repo.State.Get().IsStandby {
+		log.Err(errors.New("開始要求失敗しました"))
+		return
+	}
+	fmt.Println(Main.Start())
+	if Main.Start() {
+		err := LCD.SetJob(repo.Job.Get().Job)
+		if err != nil {
+			panic(err)
+		}
+		repo.State.IsStandby(false)
+	} else {
+		repo.State.IsStandby(true)
+		log.Err(errors.New("開始要求失敗しました"))
+		return
+	}
 }
